afed5694a7e36eb7f6012ec3adb59d82c675d351
jean heyller palomino <heyller-19@outlook.com>
2024-03-12 21:31:52 -0500

    a port is introduced for the technology model

--- src/main/java/com/pragma/OnClass/domain/model/Technology.java
+++ src/main/java/com/pragma/OnClass/domain/model/Technology.java
@@ -1,12 +1,12 @@
 package com.pragma.OnClass.domain.model;
 
-public class technology {
+public class Technology {
     private final Long id;
     private final String name;
 
     private final String description;
 
-    public technology(Long id, String name, String description) {
+    public Technology(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
